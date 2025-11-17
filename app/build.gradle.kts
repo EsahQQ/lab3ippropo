6daf22cc1a240d0da1e22af51bde36dc4a331de8
Kovalev Ivan <kovalevvanc@gmail.com>
2025-11-17 18:19:59 +0300

    fix(ci): Add missing test engine and fix checkstyle path

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,6 +12,7 @@ dependencies {
     implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine") // ВОТ ЧТО МЫ ВЕРНУЛИ
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
@@ -32,7 +33,6 @@ tasks.named<Test>("test") {
 jacoco {
     toolVersion = "0.8.10"
 }
-
 tasks.jacocoTestReport {
     dependsOn(tasks.test)
     reports {
@@ -41,21 +41,15 @@ tasks.jacocoTestReport {
         html.outputLocation.set(layout.buildDirectory.dir("reports/jacoco"))
     }
 }
-
 tasks.jacocoTestCoverageVerification {
     violationRules {
         rule {
-            // Указываем, ЧТО именно мы проверяем
             element = "CLASS"
-
-            // Добавляем исключения, чтобы не проверять App.java и ConfigLoader.java
             excludes = listOf(
                 "ru.university.lab3.App",
                 "ru.university.lab3.service.ConfigLoader"
             )
-
             limit {
-                // Требуем, чтобы покрытие ИНСТРУКЦИЙ было не менее 75%
                 counter = "INSTRUCTION"
                 value = "COVEREDRATIO"
                 minimum = "0.75".toBigDecimal()
@@ -63,12 +57,11 @@ tasks.jacocoTestCoverageVerification {
         }
     }
 }
-
 tasks.check {
     dependsOn(tasks.jacocoTestCoverageVerification)
 }
 
 checkstyle {
     toolVersion = "10.12.3"
-    configFile = rootProject.file("config/checkstyle/checkstyle.xml")
+    configFile = file("$rootDir/config/checkstyle/checkstyle.xml")
 }
